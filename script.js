637d4e90b2d3c62d1a1fbc03011dad2fb8acc635
Dilip <dilipboidya.office@gmail.com>
2024-05-11 17:17:22 +0530

    add footer

--- script.js
+++ script.js
@@ -30,37 +30,6 @@ document
   .getElementById("calculate")
   .addEventListener("click", handleCalculation);
 
-// const calculateEffectiveDielectricConstant = (Er, H, W) => {
-//   let Eff;
-
-//   if (W / H >= 1) {
-//     Eff = (Er + 1) / 2 + ((Er - 1) / 2) * Math.pow(1 + 12 * (H / W), -0.5);
-//   } else {
-//     Eff =
-//       (Er + 1) / 2 +
-//       ((Er - 1) / 2) *
-//         (Math.pow(1 + 12 * (H / W), -0.5) + 0.04 * Math.pow(1 - W / H, 2));
-//   }
-
-//   document.getElementById("dielectricResult").innerText = Eff;
-// };
-
-// // Example usage:
-// // const Er = 5; // Relative permittivity
-// // const H = 5; // Height
-// // const W = 2; // Width
-
-// const effectiveDielectricConstant = calculateEffectiveDielectricConstant(
-//   Er,
-//   H,
-//   W
-// );
-
-// document
-//   .getElementById("dielectricCalculateButton")
-//   .addEventListener("click", calculateEffectiveDielectricConstant);
-// console.log("Effective Dielectric Constant:", effectiveDielectricConstant);
-
 const calculateEffectiveDielectricConstant = () => {
   // Get the values of the input fields
   const H = parseFloat(document.getElementById("H").value);
@@ -85,3 +54,37 @@ const calculateEffectiveDielectricConstant = () => {
 document
   .getElementById("dielectricCalculateButton")
   .addEventListener("click", calculateEffectiveDielectricConstant);
+
+const calculateCharacteristicImpedance = () => {
+  // Get input values
+  const E_ff = parseFloat(document.getElementById("E_ff").value);
+  const H = parseFloat(document.getElementById("h").value);
+  const W = parseFloat(document.getElementById("w").value);
+
+  console.log(H, W, E_ff, "baal");
+  // Check if any input value is NaN
+  if (isNaN(E_ff) || isNaN(H) || isNaN(W)) {
+    document.getElementById("characteristicImpedanceResult").innerText =
+      "Invalid input";
+    return;
+  }
+
+  let Zo;
+
+  // Check if W/H >= 1
+  if (W / H >= 1) {
+    Zo =
+      (120 * Math.PI) /
+      (Math.sqrt(E_ff) * (W / H + 1.393 + (2 / 3) * Math.log(W / H + 1.444)));
+  } else {
+    Zo = (60 / Math.sqrt(E_ff)) * Math.log((8 * H) / W + (0.25 * W) / H);
+  }
+
+  // Display result
+  document.getElementById("characteristicImpedanceResult").innerText = Zo;
+};
+
+// Add event listener to the button to trigger the calculation
+document
+  .getElementById("characteristicImpedanceButton")
+  .addEventListener("click", calculateCharacteristicImpedance);
